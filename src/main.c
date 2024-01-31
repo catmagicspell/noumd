0eb8372922c5696a6abab6feab2ffb134d58d8a4
özgüshh✰ <71613978+PonpiK@users.noreply.github.com>
2024-01-31 17:59:13 +0300

    update main.c, again
    https://dar.vin/PyDML/

--- src/main.c
+++ src/main.c
@@ -3,22 +3,18 @@
 #include <systemctrl.h>
 #include <systemctrl_se.h>
 
-#define JR_RA 0x03E00008
-#define LI_V0(n) ((0x24020000) | ((n) & 0xFFFF))
-
 PSP_HEAP_SIZE_MAX();
 PSP_MODULE_INFO("noumd_prx", 0x1000, 1, 0);
 
 static void redirectFunction(int *a, void *f)
 {
 	*a = 0x08000000 | (((int)f >> 2) & 0x03FFFFFF);
-	*(a + 1) = 0;
+	*(a + 1) = 0x00000000;
 }
 
-static void makeDummyFunctionReturnZero(int *a)
+static int sceGpioPortReadPatched(void)
 {
-	*a = JR_RA;
-	*(a + 1) = LI_V0(0);
+	return *((int *)0xBE240004) & 0xFBFFFFFF;
 }
 
 static int sceUmdRegisterUMDCallBackPatched(int cbid)
@@ -29,31 +25,31 @@ static int sceUmdRegisterUMDCallBackPatched(int cbid)
 	return res;
 }
 
-static int sceGpioPortReadPatched(void)
-{
-	return *((int *)0xBE240004) & 0xFBFFFFFF;
-}
-
 static void umd_thread(SceSize args, void *argp)
 {
-	if (!sceKernelFindModuleByName("sceNp9660_driver") && !sceKernelFindModuleByName("sceIsofs_driver") && !sceKernelFindModuleByName("PRO_Inferno_Driver"))
+	int umdPresent = sceKernelFindModuleByName("sceNp9660_driver") ||
+					 sceKernelFindModuleByName("sceIsofs_driver") ||
+					 sceKernelFindModuleByName("PRO_Inferno_Driver");
+	if (!umdPresent)
 	{
 		void *sceUmdUser = sctrlHENFindFunction("sceUmd_driver", "sceUmdUser", 0xAEE7404D);
 		if (sceUmdUser)
-			redirectFunction((int)sceUmdUser, sceUmdRegisterUMDCallBackPatched);
+			redirectFunction(sceUmdUser, sceUmdRegisterUMDCallBackPatched);
 
 		void *CheckMedium = sctrlHENFindFunction("sceUmd_driver", "sceUmdUser", 0x46EBB729);
 		if (CheckMedium)
-			makeDummyFunctionReturnZero((int)CheckMedium);
+		{
+			*(int *)CheckMedium = 0x03E00008;
+			*((int *)CheckMedium + 1) = 0x24020000;
+		}
 
-		int (*IoDelDrv)(const char *) = sctrlHENFindFunction("sceIOFileManager", "IoFileMgrForKernel", 0xC7F35804);
+		void (*IoDelDrv)(const char *) = sctrlHENFindFunction("sceIOFileManager", "IoFileMgrForKernel", 0xC7F35804);
 		if (IoDelDrv)
 			IoDelDrv("umd");
 	}
-
 	void *sceGpio_driver = sctrlHENFindFunction("sceLowIO_Driver", "sceGpio_driver", 0x4250D44A);
 	if (sceGpio_driver)
-		redirectFunction((int)sceGpio_driver, sceGpioPortReadPatched);
+		redirectFunction(sceGpio_driver, sceGpioPortReadPatched);
 }
 
 int module_start(SceSize args, void *argp)
